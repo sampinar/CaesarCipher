7a8872ade903de938e50a7657b6f17d0041faa98
Sament Pinar <sampinar@gmail.com>
2020-03-03 13:47:03 +1100

    Added '__eq__' and '__repr__' methods. Only '__repr__' method is written incorrectly. Answer from edX wants us to print differently. Changing accordingly...

--- python/edx/week5/exercises/coordinate.py
+++ python/edx/week5/exercises/coordinate.py
@@ -12,6 +12,9 @@ class Coordinate(object):
     def __init__(self,x,y):
         self.x = x
         self.y = y
+    
+    def __eq__(self, other):
+        return self.x == other
 
     def getX(self):
         # Getter method for a Coordinate object's x coordinate.
@@ -22,5 +25,16 @@ class Coordinate(object):
         # Getter method for a Coordinate object's y coordinate
         return self.y
 
-    def __str__(self):
-        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+    #def __str__(self):
+    #    return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+
+    def __repr__(self):
+        return 'repr(' + str(self.x) + ')' + ' == ' + 'repr(' + str(self.y) + ')'
+
+sup = Coordinate(4, 4)
+print(sup.x)
+print(sup.y)
+
+print(sup.x == sup.y)
+
+print(sup)
