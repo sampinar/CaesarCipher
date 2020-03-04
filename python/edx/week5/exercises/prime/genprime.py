09c22ccce88f5836a61c1e2df892ed3164d339f1
Sament Pinar <sampinar@gmail.com>
2020-03-04 14:53:34 +1100

    genPrime generator function working. As you use next(), it will output prime nubmers starting from '2'. It's still infinit loop, but it's a generator function

--- python/edx/week5/exercises/prime/genprime.py
+++ python/edx/week5/exercises/prime/genprime.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
+# Exercise genPrimes from edx week5 OOP
+# As you call 'g' below, it will generate prime numbers starting from '2'. https://en.wikipedia.org/wiki/Prime_number
 
 def genPrimes():
-    ans = []
-    prime = 2
+    y = 2
 
     while True:
-        x = prime // 2
+        x = y // 2
         while x > 1:
-            if prime % x == 0:
+            if y % x == 0:
+                y += 1
                 break
             x -= 1
-            prime += 1
         else:
-            ans.append(i)
-            yield i
-            prime += 1
+            yield y
+            y += 1
 
 
 g = genPrimes()
